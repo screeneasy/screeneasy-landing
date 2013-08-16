7cf639f56bd4d42ef68400f3b0ba0a61e0e03474
Nicholas Rushton <nick.rushton@hotmail.com>
2013-08-16 10:53:41 -0700

    adding stub methods

--- app.js
+++ app.js
@@ -55,6 +55,14 @@ app.post('/subscribe', function(req,res) {
    }
 })
 
+function validEmail(email) {
+   return true;
+}
+
+function isSubscribed(email) {
+   return false;
+}
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
